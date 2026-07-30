04cd8913e70722f51be51be4063733c4937ee2f6
sashchikova05@mail.ru <sashchikova05@mail.ru>
2026-07-30 14:18:23 +0300

    change class initialization

--- src/main.py
+++ src/main.py
@@ -2,10 +2,8 @@ import sys
 import os
 project_root = os.path.abspath(os.path.join(os.getcwd(), '..'))
 sys.path.append(project_root)
-from data_base.connection import DataBase
+from preprocessing import Preprocessor
 
 if __name__ == "__main__":
-    db = DataBase("e-commerce", "liza", "postgress")
-    db.connect()
-    #print(db.execute_query('initial_queries.sql'))
-    db.close()
+    preprocessor = Preprocessor()
+    preprocessor.preprocess()
