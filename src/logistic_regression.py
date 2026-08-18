75a0233b23f84660a63d0eb4e98cd2a89519104e
sashchikova05@mail.ru <sashchikova05@mail.ru>
2026-08-18 22:29:10 +0300

    change target to bad_review

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,19 +1,43 @@
 from sklearn.linear_model import LogisticRegression
+from model import Model
+from sklearn.model_selection import GridSearchCV
 
 class Logistic_Regression(Model):
-    def __init__(self, penalty, learning_rate, max_iter, lambd, tol, random_state, test_size, n_splits):
-        super().__init__(random_state, test_size)
-        self.learning_rate = learning_rate
-        self.max_iter = max_iter
+    def __init__(self, penalty, solver, max_iter, lambd, tol, random_state, test_size, n_splits, class_weight):
+        super().__init__(random_state, test_size, n_splits)
         self.penalty = penalty
+        self.solver = solver
+        self.max_iter = max_iter
         self.lambd = lambd
         self.tol = tol
-        self.n_splits = n_splits
+        self.class_weight = class_weight
 
-    def train(self, X_train, y_train):
-        model = LogisticRegression(penalty=self.penalty, )
-        model.fit(X_train, y_train)
-        return model
+    def train_model(self, X_train, y_train):
+        model = LogisticRegression(
+            max_iter=self.max_iter,
+            class_weight=self.class_weight,
+            tol=self.tol
+        )
+        params = {
+            'penalty': self.penalty,
+            'solver': self.solver,
+            'C': self.lambd,
+        }
+        grid_search = GridSearchCV(
+            model,
+            params,
+            cv=5,
+            scoring='roc_auc',
+            n_jobs=-1,
+            verbose=1
+        )
+        grid_search.fit(X_train, y_train)
+        print(grid_search.best_estimator_)
+        return grid_search.best_estimator_
+
+    def predict_model(self, X_test, model):
+        return model.predict(X_test)
+
 
     def run_logreg(self, X, y):
         print(self.run_cross_validation(X, y))
--- src/model.py
+++ src/model.py
@@ -1,15 +1,16 @@
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, roc_auc_score
-import numpy
+import numpy as np
 from sklearn.model_selection import KFold
 
 class Model:
-    def __init__(self, random_state, test_size):
+    def __init__(self, random_state, test_size, n_splits):
         self.random_state = random_state
         self.test_size = test_size
+        self.n_splits = n_splits
 
     def cross_validation(self, X, y):
-        kf = KFold(n_splits=self.n_splits, random_state=self.random_state)
-        for train_index, test_index in zip(X, y):
+        kf = KFold(n_splits=self.n_splits, shuffle=True, random_state=self.random_state)
+        for train_index, test_index in kf.split(X):
             X_train = X.iloc[train_index]
             y_train = y.iloc[train_index]
             X_test = X.iloc[test_index]
@@ -17,11 +18,14 @@ class Model:
             yield X_train, y_train, X_test, y_test
 
     def train_and_evaluate(self, X_train, y_train, X_test, y_test):
-        self.train_model(X_train, y_train)
-        y_pred = self.predict(X_test)
+        model = self.train_model(X_train, y_train)
+        y_pred = self.predict_model(X_test, model)
         return self.calculate_metrics(y_test, y_pred)
 
     def train_model(self, X_train, y_train):
+        raise NotImplementedError
+
+    def predict_model(self, X_test):
         raise NotImplementedError
 
     def calculate_metrics(self, y_test, y_pred):
@@ -34,11 +38,16 @@ class Model:
         }
         return metrics
 
-    def average_metrics(self):
+    def average_metrics(self, metrics):
+        metric_names = ['accuracy', 'precision', 'recall', 'f1', 'ROC-AUC']
+        return {
+            metric: np.mean([m[metric] for m in metrics]) for metric in metric_names
+        }
 
 
     def run_cross_validation(self, X, y):
-        metrics = {}
+        metrics = []
         for X_train, y_train, X_test, y_test in self.cross_validation(X, y):
-            metrics.append(self.calculate_metrics)
-    return self.average_metrics(metrics)
+            metrics.append(self.train_and_evaluate(X_train, y_train, X_test, y_test))
+        return self.average_metrics(metrics)
+        #return metrics
