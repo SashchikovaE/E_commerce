966d388b463571a138f7ecd3e18d6243cb1336aa
sashchikova05@mail.ru <sashchikova05@mail.ru>
2026-08-18 22:29:31 +0300

    change target to bad_review

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -19,91 +19,134 @@ class Preprocessor():
         else:
             self.df = pd.read_sql('SELECT * FROM orders_master', conn)
 
+    def check_cols_for_encode(self):
+        # для енкода
+        print(self.df['most_common_product_category_name'].nunique())
+        print(self.df['payment_type'].unique())
+        print(self.df['customer_state'].nunique())
+        print(self.df['customer_city'].nunique())
+        print(self.df['most_common_seller_state'].nunique())
+        print(self.df['most_common_seller_city'].nunique())
+        print(self.df['product_name_length'].nunique())
+        print(self.df['product_description_length'].nunique())
+
+    def check_order_status(self):
+        pd.set_option("display.width", None)
+        # чекнуть че такое кэнслд (чювак не оплатил)
+        # unavailable это тупа удаленные
+        print(self.df[self.df['order_status'] == 'canceled'])
+        print(self.df[self.df['order_status'] == 'unavailable'])
+
     def analys_table(self):
         #print(self.df.describe())
         print(self.df.columns)
-        #print(self.df['customer_state'].nunique())
-        #print(self.df['customer_city'].nunique())
-        #pd.set_option("display.width", None)
-        #print(self.df)
+        pd.set_option("display.width", None)
+        print(self.df)
 
     def check_omissions(self):
         print(self.df.isna().sum())
-        #print(self.df[self.df['order_approved_at'].isna()])
-        #print(self.df[self.df['order_delivered_customer_date'].isna()])
-        #print(self.df[self.df['customer_lat'].isna()])
-        #print(self.df[self.df['product_category_name'].isna()])
-        #print(self.df[self.df['product_weight_g'].isna()])
-        #print(self.df[self.df['review_score'].isna()])
-        #print(self.df[self.df['total_payment'].isna()])
+        print(self.df[self.df['order_approved_at'].isna()])
+        print(self.df[self.df['order_delivered_customer_date'].isna()])
+        print(self.df[self.df['customer_lat'].isna()])
+        print(self.df[self.df['product_category_name'].isna()])
+        print(self.df[self.df['product_weight_g'].isna()])
+        print(self.df[self.df['review_score'].isna()])
+        print(self.df[self.df['total_payment'].isna()])
+        print(self.df[self.df['product_name_lenght'].isna()])
+        print(self.df[self.df['product_description_lenght'].isna()])
 
     def fill_nan_dates(self):
-        # 1. Колонки с датами
         date_cols = [
             'order_approved_at',
             'order_delivered_carrier_date',
             'order_delivered_customer_date',
             'order_estimated_delivery_date'
         ]
-        # 2. Превращаем даты в дни (чтобы можно было усреднять)
         for col in date_cols:
             self.df[f'{col}_days'] = (self.df[col] - self.df['order_purchase_timestamp']).dt.days
-        # 3. Маски для NaN
+
         nan_masks = {}
         for col in date_cols:
             nan_masks[col] = self.df[f'{col}_days'].isna()
-        # 4. Заполняем по продавцу (как в coords — по городу)
+
         for col in date_cols:
             days_col = f'{col}_days'
             seller_avg = self.df.groupby('seller_id')[days_col].mean()
             self.df.loc[nan_masks[col], days_col] = self.df.loc[nan_masks[col], 'seller_id'].map(seller_avg)
-        # 5. Если всё ещё NaN — заполняем по городу продавца (как в coords — по штату)
+
         for col in date_cols:
             days_col = f'{col}_days'
             if self.df[days_col].isna().any():
                 nan_mask = self.df[days_col].isna()
                 city_avg = self.df.groupby('seller_city')[days_col].mean()
                 self.df.loc[nan_mask, days_col] = self.df.loc[nan_mask, 'seller_city'].map(city_avg)
-        # 6. Если всё ещё NaN — заполняем по штату продавца
+
         for col in date_cols:
             days_col = f'{col}_days'
             if self.df[days_col].isna().any():
                 nan_mask = self.df[days_col].isna()
                 state_avg = self.df.groupby('seller_state')[days_col].mean()
                 self.df.loc[nan_mask, days_col] = self.df.loc[nan_mask, 'seller_state'].map(state_avg)
-        # 7. Если всё ещё NaN — глобальное среднее
+
         for col in date_cols:
             days_col = f'{col}_days'
             if self.df[days_col].isna().any():
                 nan_mask = self.df[days_col].isna()
                 global_avg = self.df[days_col].mean()
                 self.df.loc[nan_mask, days_col] = global_avg
-        # 8. Возвращаем дни обратно в даты
+
         for col in date_cols:
             days_col = f'{col}_days'
             self.df[col] = self.df['order_purchase_timestamp'] + pd.to_timedelta(self.df[days_col], unit='D')
-        # 9. Удаляем временные колонки с днями
+
         days_cols = [f'{col}_days' for col in date_cols]
         self.df = self.df.drop(columns=days_cols)
+
+    def fill_nan_product_info(self):
+        prod_cols = [
+            'product_description_lenght',
+            'product_name_lenght'
+        ]
+        for i in prod_cols:
+            nan_i_mask = self.df[i].isna()
+            avg_i = self.df.groupby('seller_id')[i].mean()
+            self.df.loc[nan_i_mask, i] = self.df.loc[nan_i_mask, 'seller_id'].map(avg_i)
+            if self.df[i].isna().any():
+                nan_i_mask = self.df[i].isna()
+                avg_i = self.df.groupby('seller_city')[i].mean()
+                self.df.loc[nan_i_mask, i] = self.df.loc[nan_i_mask, 'seller_city'].map(
+                    avg_i)
+            if self.df[i].isna().any():
+                nan_i_mask = self.df[i].isna()
+                avg_i = self.df.groupby('seller_state')[i].mean()
+                self.df.loc[nan_i_mask, i] = self.df.loc[nan_i_mask, 'seller_state'].map(
+                    avg_i)
+            if self.df[i].isna().any():
+                nan_i_mask = self.df[i].isna()
+                avg_i = self.df[i].mean()
+                self.df.loc[nan_i_mask, i] = avg_i
+                #self.df.loc[nan_name_mask, 'customer_lng'] = self.df.loc[nan_name_mask, 'customer_state'].map(
+                #    avg_customer_lng)
 
     def fill_nan_coords(self):
         coords_cols = [
             'customer_lat',
             'customer_lng',
         ]
-        nan_lat_mask = self.df['customer_lat'].isna()
-        nan_lng_mask = self.df['customer_lng'].isna()
-        avg_customer_lat = self.df.groupby('customer_city')['customer_lat'].mean()
-        avg_customer_lng = self.df.groupby('customer_city')['customer_lng'].mean()
-        self.df.loc[nan_lat_mask, 'customer_lat'] = self.df.loc[nan_lat_mask, 'customer_city'].map(avg_customer_lat)
-        self.df.loc[nan_lng_mask, 'customer_lng'] = self.df.loc[nan_lng_mask, 'customer_city'].map(avg_customer_lng)
-        if self.df['customer_lat'].isna().any():
-            nan_lat_mask = self.df['customer_lat'].isna()
-            nan_lng_mask = self.df['customer_lng'].isna()
-            avg_customer_lat = self.df.groupby('customer_state')['customer_lat'].mean()
-            avg_customer_lng = self.df.groupby('customer_state')['customer_lng'].mean()
-            self.df.loc[nan_lat_mask, 'customer_lat'] = self.df.loc[nan_lat_mask, 'customer_state'].map(avg_customer_lat)
-            self.df.loc[nan_lng_mask, 'customer_lng'] = self.df.loc[nan_lng_mask, 'customer_state'].map(avg_customer_lng)
+        for i in coords_cols:
+            nan_coord_mask = self.df[i].isna()
+            #nan_lng_mask = self.df['customer_lng'].isna()
+            avg_coord = self.df.groupby('customer_city')[i].mean()
+            #avg_customer_lng = self.df.groupby('customer_city')['customer_lng'].mean()
+            self.df.loc[nan_coord_mask, i] = self.df.loc[nan_coord_mask, 'customer_city'].map(avg_coord)
+            #self.df.loc[nan_lng_mask, 'customer_lng'] = self.df.loc[nan_lng_mask, 'customer_city'].map(avg_customer_lng)
+            if self.df[i].isna().any():
+                nan_coord_mask = self.df[i].isna()
+                #nan_lng_mask = self.df['customer_lng'].isna()
+                avg_coord = self.df.groupby('customer_state')[i].mean()
+                #avg_customer_lng = self.df.groupby('customer_state')['customer_lng'].mean()
+                self.df.loc[nan_coord_mask, i] = self.df.loc[nan_coord_mask, 'customer_state'].map(avg_coord)
+                #self.df.loc[nan_lng_mask, 'customer_lng'] = self.df.loc[nan_lng_mask, 'customer_state'].map(avg_customer_lng)
 
     def fill_nan_category(self):
         nan_category_mask = self.df['product_category_name'].isna()
@@ -116,8 +159,16 @@ class Preprocessor():
                 nan_category_mask, 'seller_id'].map(mode_category)
         if self.df['product_category_name'].isna().any():
             nan_category_mask = self.df['product_category_name'].isna()
-            mode_category = self.df['product_category_name'].mode()[0]
-            self.df.loc[nan_category_mask, 'product_category_name'] = mode_category
+            mode_category = self.df.groupby('seller_city')['product_category_name'].agg(lambda x: x.mode()[0] if len(x.mode()) > 0 else None)
+            self.df.loc[nan_category_mask, 'product_category_name'] = self.df.loc[
+                nan_category_mask, 'seller_city'
+            ].map(mode_category)
+        if self.df['product_category_name'].isna().any():
+            nan_category_mask = self.df['product_category_name'].isna()
+            mode_category = self.df.groupby('seller_state')['product_category_name'].agg(lambda x: x.mode()[0] if len(x.mode()) > 0 else None)
+            self.df.loc[nan_category_mask, 'product_category_name'] = self.df.loc[
+                nan_category_mask, 'seller_state'
+            ].map(mode_category)
 
     def fill_nan_dimensions(self):
         'один товар. продавец забыл указать'
@@ -174,12 +225,12 @@ class Preprocessor():
         self.df.loc[nan_pay_mask, 'payment_installments'] = installments_mode
 
     def fill_nan(self):
-        self.df = self.df[self.df['order_status'] != 'canceled']
-        self.df = self.df[self.df['order_status'] != 'unavailable']
+        self.df = self.df[self.df['order_status'] == 'delivered']
         self.df = self.df.dropna(subset=['product_id'])
         self.fill_nan_coords()
         self.fill_nan_dates()
         self.fill_nan_category()
+        self.fill_nan_product_info()
         self.fill_nan_dimensions()
         self.fill_nan_payment()
         self.fill_nan_review()
@@ -195,7 +246,7 @@ class Preprocessor():
         return R * c
 
     def add_new_features(self):
-        self.df['order_approval_time'] = (self.df['order_approved_at'] - self.df['order_purchase_timestamp']).dt.days
+        self.df['order_approval_time_h'] = (self.df['order_approved_at'] - self.df['order_purchase_timestamp']).dt.total_seconds() / 3600
         self.df['order_delivery_time'] = (self.df['order_delivered_customer_date'] - self.df['order_purchase_timestamp']).dt.days
         self.df['order_delay_time'] = (self.df['order_delivered_customer_date'] - self.df['order_estimated_delivery_date']).dt.days
         self.df['product_dimensions'] = self.df['product_length_cm'] * self.df['product_height_cm'] * self.df['product_width_cm']
@@ -204,76 +255,127 @@ class Preprocessor():
                                                             row['seller_lat'], row['seller_lng']),
                                                             axis=1)
         self.df['delivery_review_time'] = (self.df['review_creation_date'] - self.df['order_delivered_customer_date']).dt.days
+        self.df['review_to_answer_time_h'] = (self.df['review_answer_timestamp'] - self.df['review_creation_date']).dt.total_seconds() / 3600
+        self.df = self.df.drop([
+            'order_purchase_timestamp',
+            'order_approved_at',
+            'order_delivered_carrier_date',
+            'order_delivered_customer_date',
+            'order_estimated_delivery_date',
+            'product_length_cm',
+            'product_height_cm',
+            'product_width_cm',
+            'shipping_limit_date',
+            'review_creation_date',
+            'review_answer_timestamp',
+            'seller_lat',
+            'seller_lng',
+            'customer_lat',
+            'customer_lng'
+            ], axis=1)
 
     def group_data(self):
-        order_level = self.df.groupby(['customer_unique_id', 'order_id'], as_index=False).agg(
-            order_item_id=('order_item_id', 'count'),
-            product_category_name=('product_category_name', 'nunique'),
-            product_dimensions=('product_dimensions', 'mean'),
-            product_weight_g=('product_weight_g', 'mean'),
-            seller_city=('seller_city', 'first'),
-            seller_state=('seller_state', 'first'),
-            seller_customer_distance=('seller_customer_distance', 'mean'),
-            seller_delay=('seller_delay', 'mean'),
-            price_sum=('price', 'sum'),
-            price_mean=('price', 'mean'),
-            freight_value=('freight_value', 'mean'),
+        order_level = self.df.groupby(['order_id'], as_index=False).agg(
+            count_order_item_id=('order_item_id', 'count'),
+            customer_unique_id=('customer_unique_id', 'first'),
+            #order_status=('order_status', 'first'),
+
+            # product info
+            product_id=('product_id', 'first'),
+            product_description_length=('product_description_lenght', 'first'),
+            product_name_length=('product_name_lenght', 'first'),
+            nunique_product_category_name=('product_category_name', 'nunique'),
+            most_common_product_category_name=('product_category_name', lambda x: x.mode()[0] if len(x.mode()) > 0 else None),
+            avg_product_dimensions=('product_dimensions', 'mean'),
+            avg_product_weight_g=('product_weight_g', 'mean'),
+
+            # geo
             customer_city=('customer_city', 'first'),
             customer_state=('customer_state', 'first'),
-            order_approval_time=('order_approval_time', 'first'),
-            order_delivery_time=('order_delivery_time', 'first'),
-            order_delay_time=('order_delay_time', 'first'),
+            most_common_seller_city=('seller_city', lambda x: x.mode()[0] if len(x.mode()) > 0 else None),
+            most_common_seller_state=('seller_state', lambda x: x.mode()[0] if len(x.mode()) > 0 else None),
+            avg_seller_customer_distance=('seller_customer_distance', 'mean'),
+
+            # price
+            total_price=('price', 'sum'),
+            avg_price=('price', 'mean'),
+            avg_freight_value=('freight_value', 'mean'),
+
+            # payment info
             payment_type=('payment_type', 'first'),
             payment_installments=('payment_installments', 'first'),
-            review_score=('review_score', 'first'),
-            delivery_review_time=('delivery_review_time', 'mean')
-        )
 
-        print(order_level.columns.tolist())
-        customer_level = order_level.groupby('customer_unique_id', as_index=False).agg(
-            order_count=('order_id', 'count'),
-            customer_city=('customer_city', 'first'),
-            customer_state=('customer_state', 'first'),
-            avg_time_approval=('order_approval_time', 'mean'),
-            avg_delivery_time=('order_delivery_time', 'mean'),
-            avg_delay_time=('order_delay_time', 'mean'),
-            avg_item_count=('order_item_id', 'mean'),
-            avg_number_category=('product_category_name', 'mean'),
-            avg_product_dimension=('product_dimensions', 'mean'),
-            avg_product_weight=('product_weight_g', 'mean'),
-            most_common_seller_city=('seller_city', lambda x: x.mode()[0]),
-            most_common_seller_state=('seller_state', lambda x: x.mode()[0]),
-            avg_seller_customer_distance=('seller_customer_distance', 'mean'),
+            # order timing
+            order_approval_time_h=('order_approval_time_h', 'first'),
+            order_delivery_time=('order_delivery_time', 'first'),
+            order_delay_time=('order_delay_time', 'first'),
             avg_seller_delay=('seller_delay', 'mean'),
-            total_amount=('price_sum', 'sum'),
-            avg_price=('price_mean', 'mean'),
-            avg_freight_value=('freight_value', 'mean'),
-            number_payment_type=('payment_type', 'nunique'),
-            avg_payment_installments=('payment_installments', 'mean'),
-            avg_review_score=('review_score', 'mean'),
-            avg_delivery_review_time=('delivery_review_time', 'mean')
-        )
-        customer_level['will_return'] = (customer_level['order_count'] > 1).astype(int)
+
+            #review
+            #avg_delivery_review_time=('delivery_review_time', 'mean'),
+            #review_to_answer_time_h=('review_to_answer_time_h', 'first'),
+            review_score=('review_score', 'first'),
+            )
+        order_level['bad_review'] = (order_level['review_score'] < 4).astype(int)
+        order_level = order_level.drop(['review_score'], axis=1)
+        self.df = order_level
 
     def label_code(self, cols, mapping):
         for col in cols:
             self.df[col] = self.df[col].map(mapping)
 
-    #def one_hot_code(self):
+    def one_hot_code(self, cols):
+        for col in cols:
+            dummies = pd.get_dummies(self.df[col], prefix=col, drop_first=True)
+            dummies = dummies.astype(int)
+            self.df = pd.concat([self.df.drop(col, axis=1), dummies], axis=1, ignore_index=False)
 
-    #def normalize(self):
+    def frequency_encode(self, cols):
+        for col in cols:
+            freq = self.df[col].value_counts()
+            self.df[f'{col}_freq'] = self.df[col].map(freq)
+            self.df = self.df.drop(col, axis=1)
 
+    def vizualize_correlation_matrix(self):
+        numeric_cols = self.df.select_dtypes(include=['number'])
+        pd.set_option('display.width', None)
+        print(numeric_cols)
+        print(self.df['bad_review'].dtype)
+        plt.figure(figsize=(28.8, 19.2))
+        sns.heatmap(numeric_cols.corr(), annot=True, fmt=".2f")
+        plt.title('Correlation matrix')
+        file = Path(__file__).parent.parent / 'images/correlation_matrix.png'
+        file.parent.mkdir(exist_ok=True)
+        plt.savefig(file)
+        plt.show()
 
-    def preprocess(self):
+    def vizualize_target_dictribution(self):
+        plt.plot(self.df['bad_review'])
+        sns.countplot(data=self.df, x='bad_review')
+        file = Path(__file__).parent.parent / 'images/target_distribution.png'
+        plt.savefig(file)
+        print(self.df['bad_review'].value_counts())
+
+    def normalize(self):
+        numeric_cols = self.df.select_dtypes(include=['number']).columns
+        non_binary_cols = [
+            col for col in numeric_cols if self.df[col].nunique() > 2]
+        mean = self.df[non_binary_cols].mean(axis=0)
+        std = self.df[non_binary_cols].std(axis=0)
+        self.df[non_binary_cols] = (self.df[non_binary_cols] - mean) / (std + 1e-10)
+
+    def preprocess(self, one_hot_cols, freq_cols):
         self.analys_table()
+        #self.check_order_status()
         self.check_omissions()
         self.fill_nan()
         self.add_new_features()
-        self.analys_table()
         self.group_data()
-        self.check_omissions()
-        #self.label_code(cols, mapping)
-        #self.one_hot_code()
-        #self.normalize()
-
-
+        #self.check_omissions()
+        self.check_cols_for_encode()
+        self.vizualize_correlation_matrix()
+        self.vizualize_target_dictribution()
+        self.one_hot_code(one_hot_cols)
+        self.frequency_encode(freq_cols)
+        self.normalize()
+        self.analys_table()
